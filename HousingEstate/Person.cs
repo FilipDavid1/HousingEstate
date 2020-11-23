3ff13c52ad4e512f5f31eef44600f4af99e7d035
FilipDavid1 <filipenkodavid@gmail.com>
2020-11-23 18:51:39 +0100

    Add constuctor and metod ToString

--- HousingEstate/Person.cs
+++ HousingEstate/Person.cs
@@ -11,6 +11,18 @@ namespace HousingEstate
         private string lastName;
 
 
-       
+        public Person(int age, string name, string lastName)
+        {
+            this.age = age;
+            this.name = name;
+            this.lastName = lastName;
+
+        }
+
+
+        public override string ToString()
+        {
+            return String.Format($"Person with name {this.name} last name {this.lastName} and with age {this.age}");
+        }
     }
 }
