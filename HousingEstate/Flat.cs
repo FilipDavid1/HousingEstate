249338cf6cfb6868f6777ec3847e019024117f78
FilipDavid1 <filipenkodavid@gmail.com>
2020-11-23 20:54:14 +0100

    Add metod for Adding persons, Add constuctor and add ToString

--- HousingEstate/Flat.cs
+++ HousingEstate/Flat.cs
@@ -16,6 +16,24 @@ namespace HousingEstate
 
 
 
-       
+        public void AddPerson(Person person)
+        {
+            personList.Add(person);
+        }
+
+
+        public Flat(int numberOfFlat, int sizeOfFlat, int numberOfRooms)
+        {
+            this.numberOfFlat = numberOfFlat;
+            this.sizeOfFlat = sizeOfFlat;
+            this.numberOfRooms = numberOfRooms;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                $"Number of flat is {numberOfFlat}, size of flat is {sizeOfFlat} square meters, and number of rooms is {numberOfRooms}");
+        }
     }
 }
