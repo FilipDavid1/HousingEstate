55882dd42bed8e24926da2023757b0a18d4fbaf4
FilipDavid1 <filipenkodavid@gmail.com>
2021-01-18 14:56:04 +0100

    Create class Entrance, add fields and constructor.

---
+++ HousingEstate/Entrance.cs
@@ -0,0 +1,43 @@
+﻿namespace HousingEstate
+{
+    public class Entrance
+    {
+        private int numberOfEntrance;
+        private int numberOfFloors;
+        private int numberOfFlats;
+
+        public int NumberOfEntrance
+        {
+            get => numberOfEntrance;
+            set
+            {
+                numberOfEntrance = value < 0 ? 0 : value;
+            }
+        }
+
+        public int NumberOfFlats
+        {
+            get => numberOfFlats;
+            set
+            {
+                numberOfFlats   = value < 0 ? 0 : value;
+            } 
+        }
+
+        public int NumberOfFloors
+        {
+            get => numberOfFloors;
+            set
+            {
+                numberOfFloors = value < 0 ? 0 : value;
+            }
+        }
+
+        public Entrance(int numberOfEntrance, int numberOfFloors, int numberOfFlats)
+        {
+            this.numberOfEntrance = numberOfEntrance;
+            this.numberOfFloors = numberOfFloors;
+            this.numberOfFlats = numberOfFlats;
+        }
+    }
+}
